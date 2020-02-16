d9e3468636983e75ff2fcb69b3a5ad0500a5dd7e
Kevin Shin <kevinshin14@gmail.com>
2020-02-16 10:19:26 -0500

    Finishing up Navigation menus

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,15 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-contact-mail</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Roster</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar
-      app
-      color="red darken-4"
-      dark
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Alpha Sigma Phi - Zeta Eta Chapter - Alumni Dashboard</v-toolbar-title>
-    </v-app-bar>
-
-    <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-      
-      </v-container>
-    </v-content>
-    <v-footer
-      color="grey darken-3"
-      app
-    >
-      <span class="white--text">&copy; 2019</span>
-    </v-footer>
-  </v-app>
+  <Navigation />
 </template>
 
+
 <script>
-  export default {
-    props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: null,
-    }),
-  }
+import Navigation from './components/Navigation'
+
+export default {
+ data: () => ({}),
+ components: {
+   Navigation
+ }
+}
 </script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      clipped
+      permanent
+      app
+      overflow
+    >
+    <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar :clipped-left=true color="#730000" app>
+      <v-toolbar-title>Alpha Sigma Phi - Alumni Dashboard </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col cols="10">
+            <v-card>
+
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+
+<script>
+export default {
+ data: () => ({
+   items: [
+    { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+    { title: 'Roster', icon: 'mdi-account-box-multiple-outline' },
+    { title: 'About', icon: 'mdi-help-box' },
+   ]
+ }),
+ created () {
+      this.$vuetify.theme.dark = true
+ },
+}
+</script>
