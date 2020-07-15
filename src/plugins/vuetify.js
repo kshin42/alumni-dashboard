ec754907fa671ef0089092a024fadea9d3fb98eb
Kevin Shin <kevin.shin@patientco.com>
2020-07-15 17:41:43 -0400

    WIP Resume workshop

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,15 +4,18 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: {
-        themes: {
-          light: {
-            primary: "#21295C",
-            secondary: "#4281A4",
-            tertiary: "#84BCDA",
-            accent: "#48A9A6",
-            error: "#EA526F",
-          },
+  icons: {
+    iconfont: 'fa'
+  },
+  theme: {
+      themes: {
+        light: {
+          primary: "#21295C",
+          secondary: "#4281A4",
+          tertiary: "#84BCDA",
+          accent: "#48A9A6",
+          error: "#EA526F",
         },
       },
+    },
 });
--- src/views/ResumeWorkshop.vue
+++ src/views/ResumeWorkshop.vue
@@ -4,10 +4,10 @@
     <v-data-table
     :headers="headers"
     :items="resumes"
-    :single-expand="singleExpand"
-    :expanded.sync="expanded"
+    :expanded="expanded"
     item-key="name"
     show-expand
+    @click:row="clicked"
     class="elevation-1"
     style="margin:10px;"
   >
@@ -18,7 +18,38 @@
       </v-toolbar>
     </template>
     <template v-slot:expanded-item="{ headers, item }">
-      <td :colspan="headers.length">More info about {{ item.name }}</td>
+        <td :colspan="headers.length">{{ item.name }}
+            <v-row>
+                <v-col cols="6">
+                    <v-card>
+                        <iframe class="doc" src=""></iframe>
+                    </v-card>
+                </v-col>
+                <v-col cols="6">
+                    <h3>Feedback Card</h3>
+                    <v-divider style="margin-bottom:20px"></v-divider>
+                    <v-card class="feedbackcard">
+                        <v-card-title>Please Enter Your Feedback</v-card-title>
+                         <v-textarea style="margin:10px"
+                        :auto-grow="true">
+                      </v-textarea>
+                        <v-card-actions>
+                            <v-btn text color="accent">Submit</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                    <h3 style="margin-top:50px">Prior Feedback Given</h3>
+                    <v-divider style="margin-bottom:20px"></v-divider>
+                     <v-data-table
+                        :headers="subheaders"
+                        :items="feedback"
+                        :items-per-page="3"
+                        item-key="feedback"
+                        class="elevation-1"
+                        style="margin:10px;" dark >
+                    </v-data-table>
+                </v-col>
+            </v-row>
+        </td>
     </template>
   </v-data-table>
 </div>
@@ -32,7 +63,6 @@ export default {
     },
     data:() => ({
         expanded: [],
-        singleExpand: false,
         headers: [
           {
             text: 'Name',
@@ -44,14 +74,32 @@ export default {
           { text: 'Class', sortable: true, value: 'class' },
           { text: '# Feedback Received', sortable: true, value: 'feedNum' },
         ],
+        subheaders: [
+            {
+                text: 'Feedback',
+                align: 'start',
+                value: 'feedback',
+            }
+        ],
+        feedback: [
+            {feedback: "this was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guess"},
+            {feedback: "this wa alright I guess"},
+            {feedback: "this was alrigt I guess"},
+            {feedback: "this was alright I guehis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guess"},
+            {feedback: "this walright I guess"},
+            {feedback: "this was alright I ess"},
+            {feedback: "this was alright I this was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guessthis was alright I guess"},
+            {feedback: "this was aght I guess"},
+            {feedback: "this was alright I gs"},
+        ],
         resumes: [
           {
             name: 'John Smith',
             major: 'CS',
             class: 'First Year',
             feedNum: 24,
-            protein: 4.0,
             iron: '1%',
+
           },
           {
             name: 'Adam Knight',
@@ -78,11 +126,22 @@ export default {
         // })
     },
     methods: {
-        
+        clicked(value) {
+            if (this.expanded.includes(value)) {
+                var i = this.expanded.indexOf(value)
+                this.expanded.splice(i, 1)
+            } else {
+                this.expanded.push(value)
+            }
+        }
     }
 }
 </script>
 
 <style scoped>
-
+.doc {
+    margin-top: 10px;
+    height: 800px;
+    width: 100%;
+}
 </style>
